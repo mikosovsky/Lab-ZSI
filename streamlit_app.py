eea795f30a9899e6a9af86c75db7e262cc5dbada
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-04-26 18:29:13 +0200

    Fix: bug with response

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,4 @@ if prompt := st.chat_input("What is up?"):
         # message_placeholder.markdown(full_response)
     # Add assistant response to chat history
     # st.session_state.messages.append({"role": "assistant", "content": full_response})
-    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+    st.session_state.messages.append({"role": "assistant", "content": assistant_response.output_text})

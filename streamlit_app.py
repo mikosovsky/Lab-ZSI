7a365d36151921c6490a4990575693940eaeafd1
Mikołaj Dawczyk <mikolaj.dawczyk@gmail.com>
2025-04-26 18:22:58 +0200

    fix: deleted chunk animation

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,12 +33,13 @@ if prompt := st.chat_input("What is up?"):
             model=st.secrets["MODEL"],
             messages=st.session_state.messages
         )
-        # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+        # # Simulate stream of response with milliseconds delay
+        # for chunk in assistant_response.split():
+        #     full_response += chunk + " "
+        #     time.sleep(0.05)
+        #     # Add a blinking cursor to simulate typing
+        #     message_placeholder.markdown(full_response + "▌")
+        # message_placeholder.markdown(full_response)
     # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    # st.session_state.messages.append({"role": "assistant", "content": full_response})
+    st.session_state.messages.append({"role": "assistant", "content": assistant_response})
